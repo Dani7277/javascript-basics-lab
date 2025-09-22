3343a22a46d4f61ec725cad789458c33b2bf98cf
Dani7277 <daniextra512@gmail.com>
2025-09-22 13:55:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// Declare and use variables
+let studentName = "Alex Johnson";
+let studentAge = 20;
+let isEnrolled = true;
+
+console.log("Student Name:", studentName);
+console.log("Student Age:", studentAge);
+console.log("Is Enrolled:", isEnrolled);
+
+// Demonstrate data types
+let exampleString = "Hello, JavaScript!";
+let exampleNumber = 42;
+let exampleBoolean = false;
+let exampleArray = ["apple", "banana", "cherry"];
+let exampleObject = { firstName: "John", lastName: "Doe" };
+
+console.log("String:", exampleString);
+console.log("Number:", exampleNumber);
+console.log("Boolean:", exampleBoolean);
+console.log("Array:", exampleArray);
+console.log("Object:", exampleObject);
+
+// Use basic operators
+// Arithmetic
+console.log("5 + 3 =", 5 + 3);
+console.log("10 - 4 =", 10 - 4);
+console.log("6 * 7 =", 6 * 7);
+console.log("20 / 5 =", 20 / 5);
+
+// Comparison
+console.log("10 > 5:", 10 > 5);
+console.log("3 < 2:", 3 < 2);
+console.log("7 === '7':", 7 === "7");
+console.log("7 !== 8:", 7 !== 8);
+
+// Logical
+console.log("true && false:", true && false);
+console.log("true || false:", true || false);
+console.log("!true:", !true);
